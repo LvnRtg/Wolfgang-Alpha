5ebde4894d16338dae0c0beb536ad026cb132110
LvnRtg <ortega@lieven.fr>
2026-06-14 14:25:04 +0200

    Moved docs

--- src/lang.rs
+++ src/lang.rs
@@ -1,40 +1,4 @@
-//! The basic syntax is the natural one with usual operator precedence. A few special features are the following.
-//! - Matrices can be initialized by typing `[1, 2, 3 \ 4, 5, 6 \ 7, 8, 9]` where the rows will be `[1,2,3]`, `[4,5,6]` and `[7,8,9]` respectively.
-//!   The backslash can be used interchangeably with a semicolon `;`, even within the same matrix.
-//! - Vectors can be initialized by typing either `[1; 2; 3]` or `[1 \ 2 \ 3]` (as one would initialize a matrix with only one column).
-//! - Tuples can be initialized by typing `(1, 2, 3)`. Tuples are polymorphic but only support very few operations. They are primarily intended to support multiple
-//!   simultaneous assignments (see below); generally, the use of vectors is preferred.
-//! - Definition of constants: `identifier := expr`, where `expr` can be any expression that can be evaluated at the time of the definition.<br/>
-//!   This returns the evaluation of `expr`, so one can write e.g. `(x := 2) + 1` to obtain `3` as output and define `x` simultaneously.<br/>
-//!   If `identifier` is already a defined constant, this will re-define it and permanently suppress the old value.
-//! - Tuple assignment is supported: write e.g. `(x, y) := rhs` where `rhs` can be evaluated to a tuple of the same size. Function assignment is not allowed in this way.
-//! - Definition of functions: `f(x, y) := 2x + y`. If e.g. `x` already exists as a constant/function, this will be ignored for the sake of the function's definition.
-//!   The `x` on the RHS of the definition will always be the `x` passed as argument, not the constant.<br/>
-//!   If one wants to include a constant from the current environment, simply type `f(y) := 2x + y` where `x` is a pre-defined constant. Note that the
-//!   current value of `x` will be captured at the time of the definition; if you change `x` later on, `f` will still use its old value.
-//! - `debug` logs the entire current environment (constants + functions) to the browser's console (access via `F12`).
-//! - Test if two values are equal: `expr = other_expr` where both expressions must be evaluable to an `Object`. Very small errors are tolerated.
-//! - The same works for `<`, `<=`, `>` and `>=`. The strict comparison signs do _not_ tolerate small errors.
-//!   As for equality, two vectors/matrices of the same size satisfy a comparison iff all of their components satisfy it.
-//! - Running `lhs = rhs` where at least one of `lhs`, `rhs` contains unknown identifiers (and is thus considered a function), both sides are evaluated at every point
-//!   in `linspace(0, 1, n)`, `linspace(1, 100, n)` and `(101, ..., 100 + n)` as well as their negative counterparts. If they differ at some point, `0` is immediately
-//!   returned. If they match at all points, `1` is returned. Per default, `n = lang::evaluator::DEFAULT_TESTEQ_REPETITIONS`. One can specify `n` by using `lhs ={e} rhs`
-//!   where `e` can be any expression evaluable to a float (will then be rounded to the nearest integer). 
-//!   The same works for `<`, `<=`, `>` and `>=`.
-//! - Partially differentiate: `d/dx (x^3 + 2x + 1)` returns `3x^2 + 2` as expression. The parentheses are not needed when differentiating e.g. a monome.<br/>
-//!   The output can be stored in a function: `f(x) := d/dx ...`.<br/>
-//!   Differentiating a function with a matrix/vector as output will differentiate component-wise and return the corresponding matrix/vector-valued function.<br/>
-//!   If the differentiated function `f(x)` outputs a vector/matrix, the output will be the function `p \mapsto D_x f(p)[1]`, that is, the direction to differentiate in will be set to 1.0 by default.
-//!   This means the syntax is still accepted although not recommended.
-//! - Directionally differentiate: multiple syntaxes:
-//!     - `D_x <expr1> (expr2)[expr3]` leads to `point := {x: expr2}` and `direction := {x: expr3}`.
-//!     - `D_{x, y} <expr1> (expr2x, expr2y)[expr3x, expr3y]` leads to `point := {x: expr2x, y expr2y}` and analogously for `direction`. Analogously for any higher number of variables.
-//!     - `D f(4)[2]`: free variables are set to be the argnames of `f` (these will be the keys of the hashmap, cf. implementation).
-//!     - `D <expr> (expr_1, ..., expr_n)[expr'_1, ..., expr'_m]`: collect all unknown identifiers within `expr` into a vector in ascending alphabetic order `x_1, ..., x_l`.
-//!       If `l=m=n`, infer that these should be the keys of the hashmaps (cf. implementation). Otherwise, return `Err`.
-//! - Notice that the token `!` acts as both the `not` operator and the factorial operator. In context, one can always differentiate between the two, with one minor downside:
-//!   the syntax `x * (!y)` cannot be shortened to `x !y` (since these spaces disappear while tokenizing, one would not be able to differentiate this with `(x!) * y`).
-//! - `sum_{i=1}^n ...` acts as one would expect. The same holds for `prod`.
+//! The explanation of the syntax can be found in `README.md`.
 
 pub mod lexer;
 pub mod parser;
