ee1e469a7c5e3df325b28f81378260e94d2e5d3f
IceWizard7 <150537995+IceWizard7@users.noreply.github.com>
2026-07-11 16:34:05 +0200

    Revert "Redesign console UI with Mathematica-style cells"

--- src/js_snippets.rs
+++ src/js_snippets.rs
@@ -2,38 +2,32 @@
 
 pub const MOVE_CURSOR_TO_RIGHT_END: &str = r#"
 setTimeout(() => {
-    const input = document.getElementById("console-input");
+    const input = document.getElementById("Display 1 Input");
     input.focus();
     input.setSelectionRange(input.value.length, input.value.length);
 }, 0);
 "#;
 pub const MOVE_CURSOR_TO_LEFT_END: &str = r#"
 setTimeout(() => {
-    const input = document.getElementById("console-input");
+    const input = document.getElementById("Display 1 Input");
     input.focus();
     input.setSelectionRange(0, 0);
 }, 0);
 "#;
 pub const SELECT_UNTIL_RIGHT_END: &str = r#"
 setTimeout(() => {
-    const input = document.getElementById("console-input");
+    const input = document.getElementById("Display 1 Input");
     input.focus();
     input.setSelectionRange(input.selectionStart, input.value.length);
 }, 0);
 "#;
 pub const SELECT_UNTIL_LEFT_END: &str = r#"
 setTimeout(() => {
-    const input = document.getElementById("console-input");
+    const input = document.getElementById("Display 1 Input");
     input.focus();
     input.setSelectionRange(0, input.selectionEnd);
 }, 0);
 "#;
 pub const FOCUS_MAIN_INPUT: &str = r#"
-document.getElementById("console-input").focus();
-"#;
-/// Focuses the input unless the user has text selected (i.e. is copying output).
-pub const FOCUS_INPUT_UNLESS_SELECTING: &str = r#"
-if (window.getSelection().toString() === "") {
-    document.getElementById("console-input").focus();
-}
+document.getElementById("Display 1 Input").focus();
 "#;
