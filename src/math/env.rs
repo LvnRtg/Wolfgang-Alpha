b8edbc38adccdbd30a5abf1fc35011cc55fe7709
LvnRtg <ortega@lieven.fr>
2026-09-04 10:22:31 +0200

    refactor: cleaned up code

--- src/math/env.rs
+++ src/math/env.rs
@@ -28,14 +28,6 @@ pub trait VarStackLookup {
     fn lookup<'a>(&'a self, key: &String) -> Option<&'a Object>;
 }
 
-// #[derive(Debug)]
-// pub enum VarStack<'a> {
-//     Empty,
-//     Frame {
-//         vars: Cow<'a, HashMap<&'a String, Cow<'a, Object>>>,
-//         parent: &'a VarStack<'a>
-//     }
-// }
 pub enum VarStack<'a, 'p> {
     Empty,
     Frame {
@@ -43,34 +35,6 @@ pub enum VarStack<'a, 'p> {
         parent: &'p dyn VarStackLookup
     }
 }
-
-// impl<'a> VarStack<'a> {
-//     pub fn lookup(&self, key: &String) -> Option<&Object> {
-//         match self {
-//             VarStack::Empty => None,
-//             VarStack::Frame { vars, parent } => {
-//                 vars.get(key)
-//                 .map(|cow| cow.as_ref())
-//                 .or_else(|| parent.lookup(key))
-//             }
-//         }
-//     }
-
-//     /// Adds a frame `(ident, value)` on top of `self` and returns the result.
-//     // pub fn with<'b>(&'a self, ident: &'b String, value: &'b Object) -> VarStack<'b> where 'a: 'b {
-//     //     VarStack::Frame {
-//     //         vars: Cow::Owned(HashMap::from([(ident, Cow::Borrowed(value))])),
-//     //         parent: self
-//     //     }
-//     // }
-//     pub fn with(&'a self, ident: &'a String, value: &'a Object) -> VarStack<'a> {
-//         VarStack::Frame {
-//             vars: Cow::Owned(HashMap::from([(ident, Cow::Borrowed(value))])),
-//             parent: self
-//         }
-//     }
-
-// }
 
 impl<'a, 'p> VarStackLookup for VarStack<'a, 'p> {
     fn lookup(&self, key: &String) -> Option<&Object> {
